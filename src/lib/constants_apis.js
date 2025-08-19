6d989055478c907f547c5cfc17eecc824c58ded2
shwetag005 <shwetag.0002@gmail.com>
2025-08-19 22:28:55 +0530

    Fifth Commit

--- src/lib/constants_apis.js
+++ src/lib/constants_apis.js
@@ -1,5 +1,7 @@
 //  Your API base URL
-export const API_BASE_URL = 'http://localhost:8000/instaFinn';
+//export const API_BASE_URL = 'http://localhost:8000/instaFinn';
+
+export const API_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 //  Define your API endpoints as constants
 export const LOGIN_API = `${API_BASE_URL}/users/login`;
